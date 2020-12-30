0fe0150e2c8d414181270c91b4aa14e2aceb5540
moon gun <who307@naver.com>
2020-12-31 00:17:00 +0900

    react-redux 활용

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AddNumberRoot from './Component/AddNumberRoot';
+import DisplayNumberRoot from './Component/DisplayNumberRoot';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {number : 0}
+  
+  render() {
+
+    return (
+      <div className="App">
+        <h1>Root</h1 >
+        <AddNumberRoot></AddNumberRoot>
+        <DisplayNumberRoot></DisplayNumberRoot>
+
+      </div >
+    );
+  }
 }
+
 
 export default App;
